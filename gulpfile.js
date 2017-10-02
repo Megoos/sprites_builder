2d141b6758e7e5b6c329a28c376263c33b4bfde6
Megoos <meguskov@gmail.com>
2017-10-02 23:10:18 +0500

    generate svg sprite + scss

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+    svgSprite = require('gulp-svg-sprite'),
+    svgmin = require('gulp-svgmin'),
+    cheerio = require('gulp-cheerio'),
+    replace = require('gulp-replace');
+
+    gulp.task('svgSpriteBuild', function () {
+      return gulp.src('./icons/*.svg')
+      // minify svg
+        .pipe(svgmin({
+          js2svg: {
+            pretty: true
+          }
+        }))
+        // remove all fill, style and stroke declarations in out shapes
+        .pipe(cheerio({
+          run: function ($) {
+            $('[fill]').removeAttr('fill');
+            $('[stroke]').removeAttr('stroke');
+            $('[style]').removeAttr('style');
+          },
+          parserOptions: {xmlMode: true}
+        }))
+        // cheerio plugin create unnecessary string '&gt;', so replace it.
+        .pipe(replace('&gt;', '>'))
+        // build svg sprite
+        .pipe(svgSprite({
+          mode: {
+            symbol: {
+              sprite: "./sprite.svg",
+              render: {
+                scss: {
+                  dest:'./sass/_sprite.scss',
+                  template: "./sass/templates/_sprite_template.scss"
+                }
+              }
+            }
+          }
+        }))
+        .pipe(gulp.dest('./sprite/'));
+    });    
